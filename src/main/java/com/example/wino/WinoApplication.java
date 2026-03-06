2c8d4842d8ffffbf948a149e41db9e6eb93641c6
abdoubm56 <bamekki.abderrahmane49@gmail.com>
2026-03-06 14:34:15 +0100

    Initial commit

---
+++ src/main/java/com/example/wino/WinoApplication.java
@@ -0,0 +1,13 @@
+package com.example.wino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wino/WinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
