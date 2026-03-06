9bf1183367a4b894b0f147b92875e4adac846a97
abdoubm56 <bamekki.abderrahmane49@gmail.com>
2026-03-06 22:34:34 +0100

    create identity and family service

---
+++ identity-familly/src/test/java/com/example/identityfamilly/IdentityFamillyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.identityfamilly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityFamillyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
