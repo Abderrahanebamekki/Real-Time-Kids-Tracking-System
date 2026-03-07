cb90ed248a1863d7c131bb67ecd1d756ff5797b0
abdoubm56 <bamekki.abderrahmane49@gmail.com>
2026-03-07 15:30:49 +0100

    create Role enum

---
+++ identity-family/src/main/java/com/example/identityfamily/user/Role.java
@@ -0,0 +1,6 @@
+package com.example.identityfamily.user;
+
+public enum Role {
+    ADMIN,
+    USER
+}
--- identity-family/src/main/java/com/example/identityfamily/user/UserEntity.java
+++ identity-family/src/main/java/com/example/identityfamily/user/UserEntity.java
@@ -1,4 +1,56 @@
 package com.example.identityfamily.user;
 
-public class UserEntity {
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEntity implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String username;
+    private String password;
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
