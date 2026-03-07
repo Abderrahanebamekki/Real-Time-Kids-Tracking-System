c23ef10a2dd7d31c773e41709d808c5ad79afea3
abdoubm56 <bamekki.abderrahmane49@gmail.com>
2026-03-07 15:26:14 +0100

    add identity-family serverice module

---
+++ identity-family/src/main/java/com/example/identityfamily/IdentityFamilyApplication.java
@@ -0,0 +1,13 @@
+package com.example.identityfamily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityFamilyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityFamilyApplication.class, args);
+	}
+
+}
---
+++ identity-family/src/test/java/com/example/identityfamily/IdentityFamilyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.identityfamily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityFamilyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
