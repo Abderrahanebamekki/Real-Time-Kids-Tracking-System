676b3f756c73e9877760ecee8db9c5b5fd5e2c4a
abdoubm56 <bamekki.abderrahmane49@gmail.com>
2026-04-25 19:56:19 +0100

    create geofencing service

---
+++ geofencing-service/src/main/java/com/example/geofencingservice/GeofencingServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.geofencingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GeofencingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeofencingServiceApplication.class, args);
+    }
+
+}
