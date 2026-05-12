e894f6d9198c4bb600b92c1d3896cef70798d3dd
abdoubm56 <bamekki.abderrahmane49@gmail.com>
2026-05-12 11:00:54 +0100

    change the path for safezones

--- geofencing-service/src/main/java/com/example/geofencingservice/controller/SafeZoneController.java
+++ geofencing-service/src/main/java/com/example/geofencingservice/controller/SafeZoneController.java
@@ -11,7 +11,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/safezones")
+@RequestMapping("/geofencing/safezones")
 @RequiredArgsConstructor
 public class SafeZoneController {
     private final SafeZoneService safeZoneService;
