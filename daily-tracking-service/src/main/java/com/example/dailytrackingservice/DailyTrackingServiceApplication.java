d9d41a69f5d39c3a4c36f6e6e46878143d247d93
abdoubm56 <bamekki.abderrahmane49@gmail.com>
2026-05-20 13:55:11 +0100

    create daily-tracking-service module

---
+++ daily-tracking-service/src/main/java/com/example/dailytrackingservice/DailyTrackingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.dailytrackingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyTrackingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyTrackingServiceApplication.class, args);
+    }
+
+}
---
+++ daily-tracking-service/src/test/java/com/example/dailytrackingservice/DailyTrackingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dailytrackingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyTrackingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
