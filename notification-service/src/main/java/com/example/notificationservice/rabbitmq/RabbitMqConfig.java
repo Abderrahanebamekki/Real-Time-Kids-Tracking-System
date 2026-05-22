ab79a4f74f2d4883507a4861624d44c7a3597ec4
abdoubm56 <bamekki.abderrahmane49@gmail.com>
2026-05-22 18:12:52 +0100

    Declare vital sign queues in notification service to ensure they exist on broker

--- notification-service/src/main/java/com/example/notificationservice/rabbitmq/RabbitMqConfig.java
+++ notification-service/src/main/java/com/example/notificationservice/rabbitmq/RabbitMqConfig.java
@@ -1,6 +1,7 @@
 package com.example.notificationservice.rabbitmq;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +16,19 @@ public class RabbitMqConfig {
         return new JacksonJsonMessageConverter();
     }
 
+    @Bean
+    public Queue heartbeatQueue() {
+        return new Queue("heartbeat-queue");
+    }
 
+    @Bean
+    public Queue oxygenQueue() {
+        return new Queue("oxygen-queue");
+    }
+
+    @Bean
+    public Queue batteryQueue() {
+        return new Queue("battery-queue");
+    }
 
 }
