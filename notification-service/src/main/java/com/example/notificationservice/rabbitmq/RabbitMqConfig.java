cc8b1104a24b2a62228422b68dda201c26862409
abdoubm56 <bamekki.abderrahmane49@gmail.com>
2026-05-22 18:13:51 +0100

    Revert "Declare vital sign queues in notification service to ensure they exist on broker"
    This reverts commit ab79a4f74f2d4883507a4861624d44c7a3597ec4.

--- notification-service/src/main/java/com/example/notificationservice/rabbitmq/RabbitMqConfig.java
+++ notification-service/src/main/java/com/example/notificationservice/rabbitmq/RabbitMqConfig.java
@@ -1,7 +1,6 @@
 package com.example.notificationservice.rabbitmq;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -16,19 +15,6 @@ public class RabbitMqConfig {
         return new JacksonJsonMessageConverter();
     }
 
-    @Bean
-    public Queue heartbeatQueue() {
-        return new Queue("heartbeat-queue");
-    }
 
-    @Bean
-    public Queue oxygenQueue() {
-        return new Queue("oxygen-queue");
-    }
-
-    @Bean
-    public Queue batteryQueue() {
-        return new Queue("battery-queue");
-    }
 
 }
