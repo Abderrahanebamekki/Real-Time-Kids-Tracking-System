2e83d500e4b3419a9639c5aa4a950bf1b86cb697
abdoubm56 <bamekki.abderrahmane49@gmail.com>
2026-06-02 11:17:25 +0100

    Revert "remove eureka discovery client from all services except eureka-server"
    This reverts commit bc6bd8d3966cf9b3277ea4f2363d98057276f450.

--- geofencing-service/src/main/java/com/example/geofencingservice/GeofencingServiceApplication.java
+++ geofencing-service/src/main/java/com/example/geofencingservice/GeofencingServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.geofencingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GeofencingServiceApplication {
 
     public static void main(String[] args) {
