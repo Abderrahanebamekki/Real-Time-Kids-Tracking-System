bc6bd8d3966cf9b3277ea4f2363d98057276f450
abdoubm56 <bamekki.abderrahmane49@gmail.com>
2026-06-02 11:05:56 +0100

    remove eureka discovery client from all services except eureka-server

--- identity-family/src/main/java/com/example/identityfamily/IdentityFamilyApplication.java
+++ identity-family/src/main/java/com/example/identityfamily/IdentityFamilyApplication.java
@@ -2,10 +2,8 @@ package com.example.identityfamily;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class IdentityFamilyApplication {
 
 	public static void main(String[] args) {
